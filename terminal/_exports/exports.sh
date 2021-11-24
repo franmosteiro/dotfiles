eeb298ff19cafc5d8b6911ff713f8350d73d9066
Fran Mosteiro <fran.mosteiro@gmail.com>
2021-11-24 11:26:19 +0100

    Move nvm export from zsh here

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -14,6 +14,10 @@ export LC_ALL="en_US.UTF-8"
 # GPG troubleshotting -> https://stackoverflow.com/questions/41052538/git-error-gpg-failed-to-sign-data
 export GPG_TTY=$(tty)
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 paths=(
   "$HOME/bin"
   "$DOTFILES_PATH/bin"
