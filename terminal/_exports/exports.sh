41b1f6e0cfe54d93ac9445f120e57aaada51f3d3
Fran Mosteiro <fran.mosteiro@sequra.es>
2021-11-25 15:43:56 +0100

    Add sdkman and some path variable for jvm/scala stuff

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -1,6 +1,9 @@
-PYTHON_PATH='/usr/local/opt/python/libexec'
-RUBY_PATH='/usr/local/opt/ruby'
-RVM='$HOME/.rvm'
+PYTHON_PATH="/usr/local/opt/python/libexec"
+RUBY_PATH="/usr/local/opt/ruby"
+RVM="$HOME/.rvm"
+JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+SCALA_HOME="$HOME/.sdkman/candidates/scala/3.1.0"
+SBT_HOME="$HOME/.sdkman/candidates/sbt/1.5.5"
 
 export GEM_HOME="$HOME/.gem"
 
@@ -18,6 +21,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# SDK Man replaced GVM. Using for Groovy, Gradle, and Maven Version Management
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
 paths=(
   "$HOME/bin"
   "$DOTFILES_PATH/bin"
@@ -25,6 +32,9 @@ paths=(
   "$PYTHON_PATH/bin"
   "$RVM/bin"
   "$GEM_HOME/bin"
+  "$JAVA_HOME/bin"
+  "$SCALA_HOME/bin"
+  "$SBT_HOME/bin"
   "/bin"
   "/usr/local/bin" # This contains Brew ZSH. If it's below `/bin` it won't be executed.
   "/usr/local/opt/make/libexec/gnubin"
