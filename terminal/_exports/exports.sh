920da7f02d46bd27b8a5e2f75f907635e8e03b2d
Fran Mosteiro <fran.mosteiro@sequra.es>
2021-12-22 16:53:13 +0100

    github_token

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 PYTHON_PATH="/usr/local/opt/python/libexec"
 RUBY_PATH="/usr/local/opt/ruby"
 RVM="$HOME/.rvm"
 JAVA_HOME="$HOME/.sdkman/candidates/java/current"
 SCALA_HOME="$HOME/.sdkman/candidates/scala/current"
 SBT_HOME="$HOME/.sdkman/candidates/sbt/1.5.5"
+PYENV_ROOT="$HOME/.pyenv"
 
 export GEM_HOME="$HOME/.gem"
 
@@ -15,18 +18,22 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
 # GPG troubleshotting -> https://stackoverflow.com/questions/41052538/git-error-gpg-failed-to-sign-data
-export GPG_TTY=$(tty)
+if [ -r ~/.zshrc ]; then echo 'export GPG_TTY=$(tty)' >> ~/.zshrc; \
+  else echo 'export GPG_TTY=$(tty)' >> ~/.zprofile; fi
 
 paths=(
+  "/opt/homebrew/bin"
   "$HOME/bin"
   "$DOTFILES_PATH/bin"
   "$RUBY_PATH/bin"
+  "$PYENV_ROOT/bin"
   "$PYTHON_PATH/bin"
   "$RVM/bin"
   "$GEM_HOME/bin"
   "$JAVA_HOME/bin"
   "$SCALA_HOME/bin"
   "$SBT_HOME/bin"
+  "$(pyenv root)/shims"
   "/bin"
   "/usr/local/bin" # This contains Brew ZSH. If it's below `/bin` it won't be executed.
   "/usr/local/opt/make/libexec/gnubin"
