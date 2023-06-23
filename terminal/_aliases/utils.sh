604fedd48eb5be3a972350e4d30f69ce3f69a1a5
Fran Mosteiro <fran.mosteiro@sequra.es>
2023-06-23 09:26:23 +0200

    Update aws account access

--- terminal/_aliases/utils.sh
+++ terminal/_aliases/utils.sh
@@ -16,4 +16,10 @@ function aws-role() {
 }
 alias aws-dev='aws-role dev && kubectx dev-eks'
 alias aws-pro='aws-role pro && kubectx prod-eks'
-alias lens='open /Applications/Lens.app'
+alias aws-root='aws-role rot'
+alias aws-bono='aws-role bon'
+alias aws-fund1='aws-role sf1'
+alias aws-fund2='aws-role sf2'
+alias aws-axioma='aws-role axioma'
+alias aws-lg='aws-role logeecom'
+alias lens='open /Applications/OpenLens.app'
